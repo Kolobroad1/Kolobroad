8f257d8db1fc55b6af24c43b1f169f96e96b3228
thatStrangeGuy <thatStrangeGuy@users.noreply.github.com>
2021-03-01 16:37:12 +0200

    Немножко оптимизировал, не удержался
    Бесило что я 3жды вызывал одинаковые вещи

--- test1.js
+++ test1.js
@@ -4,28 +4,44 @@ class Human { // Обьявление классв
         this.surname = new String(surname)
         this.age = new Number(age);
         this.gender = new String(gender);
+        this.fullName = new String(this.name + ` ` + this.surname);
     }
 }
 
+
 class Coder extends Human { //Указываем что класс Coder наследует Human
-    constructor(name, surname, age, gender, mood) { //конструктор Кодера
-        super(name, surname, age, gender); //Указываем что используем в конструктор родителя Human(как суперкласс)
+    constructor(name, surname, age, gender, mood, fullName) { //конструктор Кодера
+        super(name, surname, age, gender, fullName); //Указываем что используем в конструктор родителя Human(как суперкласс)
         this.mood = new String(mood); //Добавляем собсвтенное свойство к уже сделаным родителем полям
-    }
-    get fullName() { // Достаем полное имя
-        return this.name + ` ` + this.surname;
     }
 
 };
 
-// Создаем Обьекты и выводим их в консоль
-let boozer = new Human(`Radik`, `Shevch`, 22, `Male`, `IndRabotka`);
-let Rad = new Coder(`Radik`, `Shevch`, 22, `Male`, `IndRabotka`);
-let Serg = new Coder(`Sergey`, `Dybinsk`, 22, `Male`, `UbiRabotka`);
 
-console.log(`hey,${boozer.name}, ${boozer.age}, ${boozer.gender}, youre a ${boozer.constructor.name}`);
-console.log(`Hello, my name is ${Rad.fullName}, my age is ${Rad.age}, and im ${Rad.gender},my mood is ${Rad.mood} and im a ${Rad.constructor.name},my parental class is ${Rad.constructor.__proto__.name}`) // В конце строки вызываем имя родительского класса Human из класса Coder;
-console.log(`Hello, my name is ${Serg.fullName}, my age is ${Serg.age}, and im ${Serg.gender},my mood is ${Serg.mood} and im a ${Serg.constructor.name},my parental class is ${Rad.constructor.__proto__.name}`);
-console.log(Serg);
+class boozer extends Human { //Указываем что класс Бухарь наследует Human
+    constructor(name, surname, age, gender, mood, fullName, Alcohol) { //конструктор Бухаря
+        super(name, surname, age, gender, fullName); //Указываем что используем в конструктор родителя Human(как суперкласс)
+        this.mood = new String(mood); //Добавляем собсвтенное свойство к уже сделаным родителем полям
+        this.Alcohol = Alcohol;
+    }
+};
+
+
+// Создаем Обьекты и выводим их в консоль
+let Vasia = new boozer(`Vasia`, `Petrovich`, 55, `Male`, `"Пошел ты!"`);
+Vasia.Alcohol = `Baltika 7`
+let Rad = new Coder(`Radik`, `Shevch`, 22, `Male`, `"УИИИ работка оборудование"`);
+let Serg = new Coder(`Sergey`, `Dybinsk`, 22, `Male`, `"Уииии работка тестирование!"`);
+
+
+function introduce(Object) {
+    return console.log(`Hello, my name is ${Object.fullName}, my age is ${Object.age}, and im ${Object.gender}, my mood is ${Object.mood} and im a ${Object.constructor.name}, my parental class is ${Object.constructor.__proto__.name}`)
+}
+
+introduce(Vasia);
+introduce(Rad);
+introduce(Serg);
+
+
 // Конец
 console.log(`end`)
