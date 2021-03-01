5df8e57a3d972d3cc01e207586df8337b113f7f9
thatStrangeGuy <thatStrangeGuy@users.noreply.github.com>
2021-03-01 12:48:20 +0200

    Creating first Class

--- test1.js
+++ test1.js
@@ -1,9 +1,31 @@
-function User(name) {
-    this.name = name;
-    this.Mood = `Obdolbalsya`
+class Human {
+    constructor(name, surname, age, gender) {
+        this.name = new String(name);
+        this.surname = new String(surname)
+        this.age = new Number(age);
+        this.gender = new String(gender);
+    }
 }
-let me = new User(`Radik`);
-let you = new User(`Sergey`)
 
-console.log(me);
-console.log(you);
+class Coder extends Human {
+    constructor(name, surname, age, gender, mood) {
+        super(name, surname, age, gender);
+        this.mood = mood;
+    }
+    get fullName() {
+        return this.name + ` ` + this.surname;
+    }
+
+};
+
+
+let boozer = new Human(`Radik`, `Shevch`, 22, `Male`, `IndRabotka`);
+let Rad = new Coder(`Radik`, `Shevch`, 22, `Male`, `IndRabotka`);
+let Serg = new Coder(`Sergey`, `Dybinsk`, 22, `Male`, `UbiRabotka`);
+
+console.log(`hey,${boozer.name}, ${boozer.age}, ${boozer.gender}, youre a ${boozer.constructor.name}`);
+console.log(`Hello, my name is ${Rad.fullName}, my age is ${Rad.age}, and im ${Rad.gender},my mood is ${Rad.mood} and im a ${Rad.constructor.name}`);
+console.log(`Hello, my name is ${Serg.fullName}, my age is ${Serg.age}, and im ${Serg.gender},my mood is ${Serg.mood} and im a ${Serg.constructor.name}`);
+console.log(Serg);
+
+console.log(`end`)
