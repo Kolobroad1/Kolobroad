7b5d432722d81e7f10a6c82b94ef24fbe8029290
thatStrangeGuy <thatStrangeGuy@users.noreply.github.com>
2021-03-01 13:00:52 +0200

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,5 +1,5 @@
-class Human {
-    constructor(name, surname, age, gender) {
+class Human { // Обьявление классв
+    constructor(name, surname, age, gender) { // Конструктор обьекта Human
         this.name = new String(name);
         this.surname = new String(surname)
         this.age = new Number(age);
@@ -7,18 +7,18 @@ class Human {
     }
 }
 
-class Coder extends Human {
-    constructor(name, surname, age, gender, mood) {
-        super(name, surname, age, gender);
-        this.mood = mood;
+class Coder extends Human { //Указываем что класс Coder наследует Human
+    constructor(name, surname, age, gender, mood) { //конструктор Кодера
+        super(name, surname, age, gender); //Указываем что используем в конструктор родителя Human(как суперкласс)
+        this.mood = mood; //Добавляем собсвтенное свойство к уже сделаным родителем полям
     }
-    get fullName() {
+    get fullName() { // Достаем полное имя
         return this.name + ` ` + this.surname;
     }
 
 };
 
-
+// Создаем Обьекты и выводим их в консоль
 let boozer = new Human(`Radik`, `Shevch`, 22, `Male`, `IndRabotka`);
 let Rad = new Coder(`Radik`, `Shevch`, 22, `Male`, `IndRabotka`);
 let Serg = new Coder(`Sergey`, `Dybinsk`, 22, `Male`, `UbiRabotka`);
@@ -27,5 +27,5 @@ console.log(`hey,${boozer.name}, ${boozer.age}, ${boozer.gender}, youre a ${booz
 console.log(`Hello, my name is ${Rad.fullName}, my age is ${Rad.age}, and im ${Rad.gender},my mood is ${Rad.mood} and im a ${Rad.constructor.name}`);
 console.log(`Hello, my name is ${Serg.fullName}, my age is ${Serg.age}, and im ${Serg.gender},my mood is ${Serg.mood} and im a ${Serg.constructor.name}`);
 console.log(Serg);
-
+// Конец
 console.log(`end`)
